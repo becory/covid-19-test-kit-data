ae7c345a8eb9cc17ee848f2879f41f530f4fd010
becory <13202145+becory@users.noreply.github.com>
2022-05-03 01:28:50 +0800

    remark add non update status

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pandas
 import os
 import pathlib
+import datetime
 
 def request_all_data():
     csv_data = pandas.read_csv("https://data.nhi.gov.tw/resource/Nhi_Fst/Fstdata.csv").drop(
         ["廠牌項目", "快篩試劑截至目前結餘存貨數量", "來源資料時間"], axis=1)
+    get_datetime = datetime.datetime.now()+datetime.timedelta(hours=8)
     path = pathlib.Path()
     if not os.path.isdir(str(path) + '/dist'):
         os.mkdir(str(path) + '/dist')
@@ -21,7 +23,7 @@ def request_all_data():
                 if pandas.notnull(row[key+"_y"]):
                     row_dict[key] = row[key+"_y"]
                 elif key == "醫事機構名稱":
-                    row_dict[key] = row["醫事機構名稱_x"] + "（今日資料未更新）"
+                    row_dict[key] = row["醫事機構名稱_x"]
                     is_check = True
                     check_length += 1
                     non_update.append(row["醫事機構名稱_x"])
@@ -29,7 +31,7 @@ def request_all_data():
                     row_dict[key] = row[key+"_x"]
             else:
                 if is_check is True and key == "備註":
-                    row_dict[key] =  "（今日資料未更新）"
+                    row_dict[key] =  "（截至"+get_datetime.strftime('%m/%d %H:%M:%S')+"，資料未更新）"
                 else:
                     row_dict[key] = row[key]
         new_data = pandas.concat([new_data, pandas.DataFrame([row_dict.values()], columns=csv_data.columns)], ignore_index=True)
