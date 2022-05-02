6d15e7e91584c5c3dd30dd0a1a9a7097491cfd8c
becory <13202145+becory@users.noreply.github.com>
2022-05-02 15:15:57 +0800

    fix: add json

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import pandas
+import os
+import pathlib
 
 
 def request_all_data():
-    csv_data = pandas.read_csv("https://data.nhi.gov.tw/resource/Nhi_Fst/Fstdata.csv").drop(["廠牌項目", "快篩試劑截至目前結餘存貨數量", "來源資料時間"], axis=1)
-    csv_data.to_json("all.json", orient='records', force_ascii=False)
+    csv_data = pandas.read_csv("https://data.nhi.gov.tw/resource/Nhi_Fst/Fstdata.csv").drop(
+        ["廠牌項目", "快篩試劑截至目前結餘存貨數量", "來源資料時間"], axis=1)
+    path = pathlib.Path()
+    if not os.path.isdir(str(path) + '/dist'):
+        os.mkdir(str(path) + '/dist')
+    csv_data.to_json("dist/all.json", orient='records', force_ascii=False)
 
 
 if __name__ == '__main__':
